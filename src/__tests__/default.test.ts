61b5a36a5383f202a4fe29c285d71e7d90bdc4d1
mantrai <mantra@penitence.eu>
2021-01-30 14:28:25 +0000

    QC Only

--- src/__tests__/default.test.ts
+++ src/__tests__/default.test.ts
@@ -1,7 +1,7 @@
-import DefaultRandomizer from "../default-randomizer";
+import DefaultRandomizer from '../default-randomizer';
 
 test('Default Randomizer', () => {
     let random = new DefaultRandomizer();
-    random.setSeed("1234567890");
+    random.setSeed('1234567890');
     expect(random.getSeed()).toBe('1234567890');
 });
