3364b3352a04d448107f3faf676312c2da1bcc0c
mantrai <mantra@penitence.eu>
2021-01-31 20:05:59 +0000

    Added enums and base orbit class too shared rather than stellar-nursery

---
+++ src/enums.ts
@@ -0,0 +1,58 @@
+export enum SystemType {
+    Solitary = 'Solitary System',
+    Binary = 'Binary System',
+    Trinary = 'Trinary System',
+}
+
+export enum Separation {
+    None,
+    Tight,
+    Close,
+    Moderate,
+    Distant,
+}
+
+export enum Zone {
+    Epistellar,
+    InnerZone,
+    OuterZone,
+}
+
+export enum BaseOrbitTypes {
+    Belt,
+    Dwarf,
+    Terrestrial,
+    Helian,
+    Jovian,
+    None,
+}
+
+export enum BaseHabitation {
+    None,
+    Mining,
+    Outpost,
+    Colony,
+    MajorColony,
+}
+
+export enum Score {
+    n0,
+    n1,
+    n2,
+    n3,
+    n4,
+    n5,
+    n6,
+    n7,
+    n8,
+    n9,
+    nA,
+    nB,
+    nC,
+    nD,
+    nE,
+    nF,
+    nG,
+    nX,
+    nY,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './default-randomizer';
 export * from './random-seed-factory';
 export * from './roll-results';
+export * from './enums';
